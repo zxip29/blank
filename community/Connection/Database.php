5e148e961deea439d0de4a6aef7954bbe3e95a5f
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-11-09 15:05:25 +0630

    Add setup table

--- community/Connection/Database.php
+++ community/Connection/Database.php
@@ -14,10 +14,28 @@ class Database {
             $config['password'],
             $config['databasename']
         );
+        $this->setupTable();
     }
 
     public function getConnection() {
         return $this->conn;
+    }
+    
+    private function setupTable() {
+        $sql = "CREATE TABLE IF NOT EXISTS users (
+            userid INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+            userprivate VARCHAR(255) NOT NULL,
+            username VARCHAR(255) NOT NULL,
+            device VARCHAR(255) NOT NULL,
+            status VARCHAR(255) NOT NULL,
+            creation VARCHAR(255) NOT NULL,
+            plan VARCHAR(255) NOT NULL,
+            profile VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL
+        );";
+        
+        mysqli_query($this->conn, $sql);
     }
 }
 ?>
